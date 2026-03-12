afe2691e54f46d6df5e46a5996b8dee414b89db1
Valentina866 <dvcano574@gmail.com>
2026-03-12 13:11:06 -0500

    fix: HU-01 "Closes #09"

--- src/main/java/org/fis/grupo1/registrarCoopropietarios.java
+++ src/main/java/org/fis/grupo1/registrarCoopropietarios.java
@@ -1,4 +1,17 @@
-String nombre = "Ana";
-String documento = "12345";
+package org.fis;
 
-System.out.println("Copropietario registrado: " + nombre);
+public class Copropietario {
+
+    public static void main(String[] args) {
+//declaro
+        String nombre = "Ana";
+        String documento = "12345";
+//se registra el copropietario
+        if(!nombre.equals("") && !documento.equals("")) {
+            System.out.println("Copropietario registrado: " + nombre);
+        } else {
+            System.out.println("Error: datos incompletos");
+        }
+
+    }
+}
