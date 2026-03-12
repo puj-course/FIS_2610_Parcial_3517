db750d3baebcc8dfb429a5fa329924c5f1e12e6a
Torres Vides <torres_kdayan@javeriana.edu.co>
2026-03-12 14:05:44 -0500

    fix: Añadir comentarios a consultarResultados.java "Closes #24"

--- src/main/java/org/fis/grupo1/consultarResultados.java
+++ src/main/java/org/fis/grupo1/consultarResultados.java
@@ -1,9 +1,9 @@
-h6: package org.fis;
-
+h6: package org.fis; // Se indica el package
+//La clase de consulta está asociada a la HU'06
 public class HU06Consulta {
 
     public static void main(String[] args) {
-
+        //Se muestra por pantalalla resultados simulados de votación
         System.out.println("Resultados finales");
         System.out.println("Carlos: 5 votos");
         System.out.println("Maria: 3 votos");
