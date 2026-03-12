35fe39911861946e00c5abe949c338d466d91bd0
Torres Vides <torres_kdayan@javeriana.edu.co>
2026-03-12 13:04:12 -0500

    fix: Complementar lógica para consulta y auditoria de resultados "Closes #24"

---
+++ src/main/java/org/fis/grupo1/consultarResultados.java
@@ -0,0 +1,12 @@
+h6: package org.fis;
+
+public class HU06Consulta {
+
+    public static void main(String[] args) {
+
+        System.out.println("Resultados finales");
+        System.out.println("Carlos: 5 votos");
+        System.out.println("Maria: 3 votos");
+
+    }
+}
