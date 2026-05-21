887cceeea5885a07fe22ae9f0d6b3b22d1c366ad
vanexalram <vramoscapera@gmail.com>
2026-05-21 12:32:10 -0500

    Candidato
    Added Candidato class with properties and methods for candidate details.

---
+++ src/main/java/org/fis/grupo1/Parcial 2/Candidato.java
@@ -0,0 +1,35 @@
+package org.fis.grupo1.Parcial2;
+
+  public class Candidato {
+
+    private String nombre;
+    private String partidoPolitico;
+    private String cargo;
+
+    public Candidato() {
+    }
+    public string getnombre() {
+        return   nombre;
+    }
+    public void setnombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getpartidoPolitico() {
+        return partidoPolitico;
+    }
+    public void setpartidoPolitico (string partidoPolitico) {
+        this.partidoPolitico = partidoPolitico;
+    }
+    public String getcargo() {
+        return cargo;
+    }
+    public void setcargo(string cargo) {
+        this.cargo = cargo);
+    }
+
+    ublic void mostrarCandidato() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Partido: " + partidoPolitico);
+        System.out.println("Cargo: " + cargo);
+    }
+}
