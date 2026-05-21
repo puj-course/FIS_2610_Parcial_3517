d6163f3f6f09354f890734f85ca35bbe5baf2a10
rodriguezdiazandres8 <rodriguezdiaz.andres8@gmail.com>
2026-05-21 12:28:50 -0500

    Update AlertaRegistro.java

--- src/main/java/org/fis/grupo1/alertas/AlertaRegistro.java
+++ src/main/java/org/fis/grupo1/alertas/AlertaRegistro.java
@@ -1,35 +1,24 @@
 package org.fis.grupo1.alertas;
+
 import org.fis.grupo1.Observer;
 import org.fis.grupo1.ResultadoElectoral;
 
 public class AlertaRegistro implements Observer {
 
+    private EstrategiaAlerta estrategiaAlerta;
+
+    public AlertaRegistro(EstrategiaAlerta estrategiaAlerta) {
+        this.estrategiaAlerta = estrategiaAlerta;
+    }
+
+    public void setEstrategiaAlerta(EstrategiaAlerta estrategiaAlerta) {
+        this.estrategiaAlerta = estrategiaAlerta;
+    }
+
     @Override
-    public void actualizar(
-            ResultadoElectoral resultado
-    ) {
+    public void actualizar(ResultadoElectoral resultado) {
 
-        System.out.println(
-                "\n=== ALERTA ==="
-        );
+        estrategiaAlerta.ejecutar(resultado);
 
-        System.out.println(
-                "Nuevo registro realizado:"
-        );
-
-        System.out.println(
-                "Candidato: "
-                + resultado.getCandidato()
-        );
-
-        System.out.println(
-                "Votos: "
-                + resultado.getVotos()
-        );
-
-        System.out.println(
-                "Mesa: "
-                + resultado.getMesa()
-        );
     }
 }
