2bb2ef68db0c21272b92acda8b45a2874e62c0c7
solonlosada2006 <solon.losada2006@gmail.com>
2026-05-21 12:47:35 -0500

    Update EstadoProcesoElectoral.java

--- src/main/java/org/fis/grupo4/State/EstadoProcesoElectoral.java
+++ src/main/java/org/fis/grupo4/State/EstadoProcesoElectoral.java
@@ -1,1 +1,23 @@
+package org.fis.grupo4;
 
+import org.fis.grupo4.state.*; 
+
+public class ProcesoElectoral {
+    private EstadoProceso estadoActual;
+    public ProcesoElectoral() {
+        this.estadoActual = new EstadoCreado();
+    }
+    public void setEstado(EstadoProceso nuevoEstado) {
+        this.estadoActual = nuevoEstado;
+        System.out.println("Estado cambiado a: " + nuevoEstado.getClass().getSimpleName());
+    }
+    public void ejecutarInicio() {
+        estadoActual.iniciar(this);
+    }
+    public void ejecutarVotacion() {
+        estadoActual.registrarVoto(this);
+    }
+    public void ejecutarFinalizacion() {
+        estadoActual.finalizar(this);
+    }
+}
