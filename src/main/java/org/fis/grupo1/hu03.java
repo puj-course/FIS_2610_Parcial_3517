32c6425a009749d7643cde04a2f7989db85c0da6
sofia-sierra2 <sofi.sierrag06@gmail.com>
2026-05-21 11:36:32 -0500

    fix: codigo de mesa de votación

--- src/main/java/org/fis/grupo1/hu03.java
+++ src/main/java/org/fis/grupo1/hu03.java
@@ -1,3 +1,30 @@
-String candidato = "Carlos";
+package org.fis;
 
-System.out.println("Candidato registrado: " + candidato);
+public class MesaVotacion {
+
+    private int numeroMesa;
+    private int censoVotantes;
+
+    // Constructor
+    public MesaVotacion(int numeroMesa, int censoVotantes) {
+        this.numeroMesa = numeroMesa;
+        this.censoVotantes = censoVotantes;
+    }
+
+    // Método básico
+    public void mostrarMesa() {
+        System.out.println("Mesa número: " + numeroMesa);
+        System.out.println("Censo aproximado: " + censoVotantes);
+    }
+
+    public static void main(String[] args) {
+
+        MesaVotacion mesa =
+                new MesaVotacion(
+                        12,
+                        350
+                );
+
+        mesa.mostrarMesa();
+    }
+}
