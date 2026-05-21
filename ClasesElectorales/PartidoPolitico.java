0ba7bcfa1ac7bd596964f9ae80c7b33cad567185
Lucas Rincon Mican <114701177+Lcks07@users.noreply.github.com>
2026-05-21 12:26:03 -0500

    Create PartidoPolitico.java

---
+++ ClasesElectorales/PartidoPolitico.java
@@ -0,0 +1,9 @@
+public class PartidoPolitico {
+    private String nombre;
+    private String sigla;
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getSigla() { return sigla; }
+    public void setSigla(String sigla) { this.sigla = sigla; }
+}
