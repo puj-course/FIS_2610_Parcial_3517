f6aa503f3aa6b0a0a44de85097ddccd876ad9414
vanexalram <vramoscapera@gmail.com>
2026-05-21 12:35:21 -0500

    Prueba candidato

---
+++ src/main/java/org/fis/grupo1/Parcial 2/Candidato_test.java
@@ -0,0 +1,34 @@
+package org.fis;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CandidatoTest {
+
+    // Caso normal
+    @Test
+    public void testCrearCandidato() {
+
+     Candidato candidato = new Candidato(
+                        "Laura",
+                        "Partido Verde",
+                        "Presidencia"
+                );
+
+        assertEquals("Laura", candidato.getNombre());
+    }
+
+    // Caso negativo
+    @Test
+    public void testNombreVacio() {
+
+        Candidato candidato =
+                new Candidato(
+                        "",
+                        "Partido Verde",
+                        "Presidencia"
+                );
+
+        assertEquals("", candidato.getNombre());
+    }
+}
