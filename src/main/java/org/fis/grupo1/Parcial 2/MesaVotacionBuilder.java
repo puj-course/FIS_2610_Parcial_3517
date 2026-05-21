70419c7d1e873f420116a3b51a36244df1e1b492
sofia-sierra2 <sofi.sierrag06@gmail.com>
2026-05-21 12:07:00 -0500

    feat: Implementar patrón builder para mesa de votación

--- src/main/java/org/fis/grupo1/Parcial 2/MesaVotacionBuilder.java
+++ src/main/java/org/fis/grupo1/Parcial 2/MesaVotacionBuilder.java
@@ -1,1 +1,33 @@
+package org.fis.grupo1.Parcial2;
+import org.fis.grupo1.Parcial2.Mesa;
+
+
+public class MesaVotacionBuilder {
+
+    private int numeroMesa;
+    private int censoVotantes;
+
+     public MesaVotacionBuilder(int numeroMesa, int censoVotantes) {
+
+        this.numeroMesa = numeroMesa;
+        this.censoVotantes = censoVotantes;
+
+        this.mesa = new Mesa();
+    }
+
+      @Override
+    public void buildIdMesa() {
+        mesa.setIdMesa(idMesa);
+    }
+
+    @Override
+    public void buildCensoVotantes() {
+        mesa.setCensoVotantes(censoVotantes);
+    }
+
+    @Override
+    public Mesa getResultadosMesa() {
+        return mesa;
+    }
+}
 
