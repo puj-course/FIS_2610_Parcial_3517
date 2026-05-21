fc835f2556306dc88b88852ae3b34f7848cb72fc
Lucas Rincon Mican <114701177+Lcks07@users.noreply.github.com>
2026-05-21 12:05:36 -0500

    Create Mesa.java
    Representa una mesa de un puesto de votación.

---
+++ ClasesElectorales/Mesa.java
@@ -0,0 +1,22 @@
+
+public class Mesa {
+    
+    private int Nmesa;
+    private long CensoAproximado;
+
+    public int getNmesa() {
+        return Nmesa;
+    }
+
+    public void setNmesa(int nmesa) {
+        Nmesa = nmesa;
+    }
+
+    public long getCensoAproximado() {
+        return CensoAproximado;
+    }
+
+    public void setCensoAproximado(long censoAproximado) {
+        CensoAproximado = censoAproximado;
+    }
+}
