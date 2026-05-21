8aaccad2237eaef261f974f25134459cdf469c1b
Lucas Rincon Mican <114701177+Lcks07@users.noreply.github.com>
2026-05-21 12:26:22 -0500

    Create Candidato.java

---
+++ ClasesElectorales/Candidato.java
@@ -0,0 +1,12 @@
+public class Candidato {
+    private String cedula;
+    private String nombre;
+    private PartidoPolitico partido;
+
+    public String getCedula() { return cedula; }
+    public void setCedula(String cedula) { this.cedula = cedula; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public PartidoPolitico getPartido() { return partido; }
+    public void setPartido(PartidoPolitico partido) { this.partido = partido; }
+}
